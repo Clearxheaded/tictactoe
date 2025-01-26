b4cea4fb2eac761c3203d46f29bcbadf06e8bc8b
Rus Windows <7777x777x7777@gmail.com>
2025-01-25 21:00:42 -0500

    Add the game controller

--- script.js
+++ script.js
@@ -26,3 +26,47 @@ const Player = (name, marker) => {
         getMarker: () => marker,
     };
 };
+
+const GameController = (() => {
+    let player1, player2;
+    let currentPlayer;
+    let gameOver = false;
+
+    const initializeGame = (name1, name2) => {
+        player1 = Player(name1, 'X');
+        player2 = Player(name2, 'O');
+        currentPlayer = player1;
+        gameOver = false;
+        Gameboard.resetBoard();
+    };
+
+    const playTurn = (index) => {
+        if (gameOver || !Gameboard.updateBoard(index, currentPlayer.getMarker())) {
+            return;
+        }
+
+        if (checkWinner()) {
+            gameOver = true;
+            console.log(`${currentPlayer.getName()} wins!`);
+        } else if (checkTie()) {
+            gameOver = true;
+            console.log("It's a tie!");
+        } else {
+            switchTurn();
+        }
+    };
+
+    const switchTurn = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const checkWinner = () => {
+        return false;
+    };
+
+    const checkTie = () => {
+        return Gameboard.getBoard().every(cell => cell !== '');
+    };
+
+    return { initializeGame, playTurn };
+})();
